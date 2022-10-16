6509ba201fbe98fe1048dfc67d1fef9c330ba658
salpreh <salva.perez46@gmail.com>
2022-10-16 20:25:56 +0200

    Base initializr template

---
+++ src/main/java/com/salpreh/resillencews/ResillencewsApplication.java
@@ -0,0 +1,13 @@
+package com.salpreh.resillencews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResillencewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResillencewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salpreh/resillencews/ResillencewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salpreh.resillencews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResillencewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
